e77584b277cd782f7d2e1cc123765c7a613054f0
busyhe <busyhe@qq.com>
2025-02-11 20:50:56 +0800

    feat: update site data

--- lib/notion/pages.ts
+++ lib/notion/pages.ts
@@ -14,12 +14,10 @@ export default function getAllPageIds(
   try {
     if (viewIds && viewIds.length > 0) {
       const view = collectionQuery[collectionId][viewIds[0]]
-      console.debug('[DEBUG__~/Workspace/projects/next-web-nav/lib/notion/pages.ts__view.table_groups.results]', view.table_groups.results)
       const groups = view.table_groups.results
         .filter((group: { value: { value: any } }) => group.value.value)
         .map((group: { value: { value: { value: string } } }) => {
           const title = group.value.value.value
-          console.debug(title, view[`results:text:${title}`].blockIds)
           return {
             title,
             items: view[`results:text:${title}`].blockIds
@@ -77,6 +75,7 @@ export const getSites = async ( ) => {
 
   const recordMap = await api.getPage(pageId, {
     fetchCollections: true,
+    fetchMissingBlocks: true,
     kyOptions: {
       timeout: 60000
     }
